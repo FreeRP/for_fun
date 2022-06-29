6314301ffd5c03dd36cd5e71f6de1f29b15a8367
Alexey <math1993@mail.ru>
2022-06-29 22:30:15 +0300

    get mode var from package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const prod = process.env.NODE_ENV === 'production';
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: prod ? 'production' : 'development',
-  entry: "./src/app.tsx",
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist/',
-    hashFunction: "sha256" // Fix error: "digital envelope routines::unsupported"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        resolve: {
-          extensions: ['.ts', '.tsx'],
-        },
-        use: 'ts-loader',
+module.exports = (env, argv) => {
+  if (argv.mode === 'development')
+    return {
+      mode: 'development',
+      entry: "./src/app.tsx",
+      output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist/',
+        hashFunction: "sha256" // Fix error: "digital envelope routines::unsupported"
       },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      module: {
+        rules: [
+          {
+            test: /\.(ts|tsx)$/,
+            exclude: /node_modules/,
+            resolve: {
+              extensions: ['.ts', '.tsx'],
+            },
+            use: 'ts-loader',
+          },
+          {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+          },
+        ]
       },
-    ]
-  },
-  devtool: prod ? undefined : 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/templates/index.html',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+      devtool: 'source-map',
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: 'src/templates/index.html',
+        }),
+        new MiniCssExtractPlugin(),
+      ],
+    };
 };
